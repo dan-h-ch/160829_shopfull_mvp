095c6113f2868e68568934c4b9fb17303b33ec9d
dan-h-ch <chang.daniel@gmail.com>
2016-08-29 16:55:18 -0700

    Configuration updates.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var app = require('./server.js');
+
+
+var port = process.env.PORT || 4568;
+
+app.listen(port);
+
+console.log('Server now listening on port ' + port);
+
+
 ReactDOM.render(<App />, document.getElementById("app"))
--- server/server.js
+++ server/server.js
@@ -3,11 +3,21 @@ var app = express();
 var path = require("path")
 
 // WOW THIS IS UGLY!!!!
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   console.log(req.url)
   res.sendFile(path.join(__dirname + "/.." + req.url))
 });
 
-app.set("port", 8080)
+app.get('/compiled/*', function(req, res) {
+  console.log(req.url)
+  res.sendFile(path.join(__dirname + "/.." + req.url))
+});
 
-app.listen(8080)
+app.get('/node_modules/*', function(req, res) {
+  console.log(req.url)
+  res.sendFile(path.join(__dirname + "/.." + req.url))
+});
+
+var port = process.env.PORT || 4568;
+
+app.listen(port)
